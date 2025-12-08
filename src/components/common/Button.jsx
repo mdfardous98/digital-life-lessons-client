a5b7c50f46f949f42c6e913b0686ab944f8b1fcf
mdfardous98 <mdjfardous@gmail.com>
2025-12-09 00:22:55 +0600

    added Loader,Button

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,8 @@
+const Button = ({ children, className, ...props }) => {
+  return (
+    <button className={`btn btn-primary ${className}`} {...props}>
+      {children}
+    </button>
+  );
+};
+export default Button;
